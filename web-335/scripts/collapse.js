93637e160f8ec637929568cb40cb82b74cf326fe
Alan Edwards <alan.edwards1103@gmail.com>
2019-05-14 09:45:38 -0400

    First commit

---
+++ web-335/scripts/collapse.js
@@ -0,0 +1,83 @@
+/*
+============================================
+; Author: Alan Edwards
+; Date:   9 May 2019
+; Description: Handler
+;===========================================
+*/ 
+
+/**
+ * The main jQuery function which activates the
+ * toggelCard function when its respective button
+ * is pressed.
+ */
+$(function() {
+    $(".expand0").click( x => {toggleCard(0)});
+    $(".expand1").click( x => {toggleCard(1)});
+    $(".expand2").click( x => {toggleCard(2)});
+    
+    $(".half-circle").mouseenter(function(){
+        $(".half-circle").css({
+            "animationName": "expandCard",
+            "animationDuration": "0.5s",
+            "animationIterationCount": "1",
+            "animationFillMode": "forwards"
+        });
+        $(".profile-card").css({
+            "animationName": "pushDown",
+            "animationDuration": "0.5s",
+            "animationIterationCount": "1",
+            "animationFillMode": "forwards"
+        });
+        $(".menu-item-container").css({
+            "animationName": "fadeIn",
+            "animationDuration": "0.5s",
+            "animationIterationCount": "1",
+            "animationFillMode": "forwards",
+            "display": "block"
+        })
+    });
+    $(".half-circle").mouseleave(function(){
+        $(".half-circle").css({
+            "animationName": "collapseCard",
+            "animationDuration": "0.5s",
+            "animationIterationCount": "1",
+            "animationFillMode": "forwards"
+        });
+        $(".profile-card").css({
+            "animationName": "pullUp",
+            "animationDuration": "0.5s",
+            "animationIterationCount": "1",
+            "animationFillMode": "forwards"
+        })
+        $(".menu-item-container").css({
+            "animationName": "fadeOut",
+            "animationDuration": "0.5s",
+            "animationIterationCount": "1",
+            "animationFillMode": "forwards",
+            "display": "none"
+        })
+        $(".tip").css({
+            "animationName": "fadeOut",
+            "animationDuration": "0.5s",
+            "animationIterationCount": "1",
+            "animationFillMode": "forwards",
+            "display": "none"
+        })
+    });
+});
+/**
+ * Toggles the card for the respecitve item. Toggles the elements
+ * css between collapse and visible. 
+ * @param {*} expandNumber 
+ */
+function toggleCard( expandNumber ){
+    var toggleCardElements = [".expandCard0", ".expandCard1", ".expandCard2"];
+
+    if ( $(toggleCardElements[expandNumber]).css("display") == "none"){
+        $(toggleCardElements[expandNumber]).css("display", "inline");
+    }
+    else{
+        $(toggleCardElements[expandNumber]).css("display", "none");
+    }
+}
---
+++ web-335/scripts/linkObf.js
@@ -0,0 +1,81 @@
+/*
+============================================
+; Author: Alan Edwards 
+; Date:   9 May 2019
+; Description: Obfuscates Links i
+; 
+; Hex to String function (function f()) from Delan Azabani:
+; https://stackoverflow.com/questions/3745666/how-to-convert-from-hex-to-ascii-in-javascript
+;===========================================
+*/ 
+/**
+ * The main jQuery function which will call the appropriate function when
+ * a element is selected.
+ */
+$(function(){
+    $("#messenger").click(x =>{LightBox.builder("fs3f345");});
+    $("#email").click(x =>{LightBox.builder("fhf389ffh9832nf2");});
+});
+
+var LightBox = {
+    /**
+     * Creates the gray cover over the entire page.
+     */
+    createCover: function(){
+        $("body").append("<div class='cover'></div>");
+    },
+    /**
+     * Removes both the modal and the cover.
+     */
+    destroy: function(){
+        $(".cover").remove();
+        $(".modalCustom").remove();
+    },
+    /**
+     * Creates the modal and passes in the text that has been deobfuscated
+     * so that it can be displayed to the user. 
+     */
+    createModal: function( deobfuscatedText ){
+        $("body").append("<div class='modalCustom'><p value='" + deobfuscatedText + "' id='text'>" + deobfuscatedText + "</p>" +
+            "<div class='linkContainer'><a href='#' class='modalLink' onclick='LightBox.destroy();return false;' id='close'>Close</a></div></div>"
+        );
+    },
+    /**
+     * Builds the calls the createCover and
+     * creatModal functions and calls the 
+     * deobfuscate function and passes the results
+     * to the createModal function
+     */
+    builder: function( itemSelector ){
+        this.createCover();
+        this.createModal(this.deobfuscate(itemSelector));
+    },
+    /**
+     * Takes the input ( a unique ID) and determines which
+     * string to return based on length comparison. Honestly,
+     * this is overkill.
+     */
+    deobfuscate: function( input ){
+        var fj32342f32 = "62726f776e6a7430314073756e796a63632e656475"
+        var rj342hfie = "68747470733a2f2f6d2e6d652f6a617265642e62726f776e2e31363536"
+        if( input.length > 10){
+            console.log(this.f(fj32342f32));
+            return this.f(fj32342f32);
+        }else{
+            return this.f(rj342hfie);
+        }
+    },
+    /**
+     * A function which returns a string after
+     * decoding the hex value. Based on an answer
+     * by Delan Azabani from Stack Exchange (link at top). 
+     */
+    f: function(fwcv){
+        var fowfv23 = fwcv.toString();
+        var s = "";
+        for(var q = 0; q < fowfv23.length; q +=2){
+            s += String.fromCharCode(parseInt(fowfv23.substr(q, 2), 16));
+        }
+        return s;
+    }
+};
